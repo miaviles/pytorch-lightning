b7a687092a697fca8b8ddfeef4ef0773a464404c
Jirka Borovec <jirka.borovec@seznam.cz>
2021-02-05 00:57:14 +0100

    wip

--- pytorch_lightning/callbacks/__init__.py
+++ pytorch_lightning/callbacks/__init__.py
@@ -21,7 +21,7 @@ from pytorch_lightning.callbacks.lr_monitor import LearningRateMonitor
 from pytorch_lightning.callbacks.model_checkpoint import ModelCheckpoint
 from pytorch_lightning.callbacks.progress import ProgressBar, ProgressBarBase
 from pytorch_lightning.callbacks.pruning import ModelPruning
-from pytorch_lightning.callbacks.quantization import QuantizationAwareTraining, StaticModelQuantization
+from pytorch_lightning.callbacks.quantization import QuantizationAwareTraining
 
 __all__ = [
     'BackboneFinetuning',
@@ -36,7 +36,6 @@ __all__ = [
     'ProgressBar',
     'ProgressBarBase',
     'ModelPruning',
-    'StaticModelQuantization',
     'QuantizationAwareTraining',
 ]
 
